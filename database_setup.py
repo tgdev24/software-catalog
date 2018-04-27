a9b1490c16a4566d45799b8ed9f86f6bd12704f4
Tsenddavaa Ganbat <tsenddavaa@sdfutures.org>
2018-04-26 18:37:45 -0700

    hello2

--- database_setup.py
+++ database_setup.py
@@ -7,4 +7,37 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class
+class Fields(Base):
+	__tablename__ = 'specialties'
+	id = Column(Integer, primary_key= True)
+	name = Column(String(250), nullable= False)
+
+	@property
+	def serialize(self):
+		return{
+			'id': self.id,
+			'name': self.name
+		}
+
+class MenuItem(Base):
+	__tablename__ = 'menu_item'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable= False)
+	description = Column(String(1000))
+	website = Column(String(500))
+	image = Column(String(500))
+	specialty_id = Column(Integer, ForeignKey('specialties.id'))
+	specialty = relationship(Fields)
+
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name, 
+			'description': self.description,
+			'website': self.website,
+			'image': self.image,
+		}
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, Fields, MenuItem
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/fields")
+def fields():
+	fields = session.query(Fields).all()
+	return render_template('fields.html', fields = fields)
 
 if __name__ == '__main__':
 	app.debug = True
