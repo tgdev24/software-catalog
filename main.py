a451ab9aba17dec0654cf86cfd0e6fdc538a7116
Tsenddavaa Ganbat <tsenddavaa@sdfutures.org>
2018-08-15 18:04:02 -0700

    hello

--- main.py
+++ main.py
@@ -389,6 +389,6 @@ def deleteMenuItem(id, menu_id):
 		return render_template('deleteMenuItem.html', item=itemToDelete)
 
 if __name__ == '__main__':
-	app.secret_key = 'super_secret_key'
+	app.secret_key = 'software_catalog'
 	app.debug = True
 	app.run(host='0.0.0.0', port=5000)
